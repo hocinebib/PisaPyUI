166234311f33504324b7a76355327b7ce5116aea
hocinebib <hocine.meraouna@gmail.com>
2021-06-21 19:43:54 +0200

    separated in tabs

--- scr/PisaUI.py
+++ scr/PisaUI.py
@@ -6,49 +6,22 @@ ttk_style = 'alt'
 sg.change_look_and_feel('DarkBlue')
 
 
-file_list_column = [
+tab1 = [
 
-    [
+    [sg.Text("Naccess bin path"),
+     sg.In(size=(25, 1), enable_events=True, key="-FOLDER1-"),
+     sg.FolderBrowse(size=(13, 1)),],
 
-        sg.Text("Naccess bin path"),
+    [sg.Text("pdb files folder"),
+     sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+     sg.FolderBrowse(size=(13, 1)),],
 
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER1-"),
+    [sg.Text(" "*15),
+     sg.Listbox(values=[], enable_events=True, size=(25, 4), key="-FILE LIST-"),
+     sg.Button('Submit folder', size=(13, 1), key='-Submit files-', use_ttk_buttons=True),],
 
-        sg.FolderBrowse(size=(13, 1)),
-
-    ],
-
-    [
-
-        sg.Text("pdb files folder"),
-
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-
-        sg.FolderBrowse(size=(13, 1)),
-
-    ],
-
-    [
-        
-        sg.Text(" "*15),
-
-        sg.Listbox(
-
-            values=[], enable_events=True, size=(25, 4), key="-FILE LIST-"
-
-        ),
-
-        sg.Button('Submit folder', size=(13, 1), key='-Submit files-', use_ttk_buttons=True),
-
-    ],
-
-    [
-
-        sg.Text('Enter pdb id    '), sg.InputText(size=(25, 1), key="-PDB ID-"),
-
-        sg.Button('Submit id', size=(13, 1), key='-Submit id-', use_ttk_buttons=True),
-
-    ],
+    [sg.Text('Enter pdb id    '), sg.InputText(size=(25, 1), key="-PDB ID-"),
+     sg.Button('Submit id', size=(13, 1), key='-Submit id-', use_ttk_buttons=True),],
 
     [sg.Text(' '*10)],
 
@@ -64,81 +37,47 @@ file_list_column = [
 
 ]
 
-image_viewer_column = [
+
+tab2 = [
 
     [sg.Text("PisaPy is a Python Wrapper for PDBePISA")],
-    [sg.Output(size=(55,30), key='-OUTPUT-')],
+    [sg.Output(size=(70,30), key='-OUTPUT-')],
     [sg.Button('Clear', use_ttk_buttons=True), sg.Button('Exit', use_ttk_buttons=True)]
 
 ]
 
-layout = [
+layout = [[sg.TabGroup([[sg.Tab('PisaPy Params', tab1), sg.Tab('Shell', tab2)]])]] 
 
-    [
-
-        sg.Column(file_list_column),
-
-        sg.VSeperator(),
-
-        sg.Column(image_viewer_column),
-
-    ]
-
-]
-
-# Create the window
 window = sg.Window("PisaPy", layout, ttk_theme=ttk_style)
 
 
-# Create an event loop
 while True:
-
     event, values = window.read()
 
     if event == "Exit" or event == sg.WIN_CLOSED:
-
         break
 
-
     if event == "-FOLDER-":
-
         folder = values["-FOLDER-"]
-
         try:
-
-            # Get list of files in folder
-
             file_list = os.listdir(folder)
-
         except:
-
             file_list = []
 
-
-        fnames = [
-
-            f
-
-            for f in file_list
-
-            if os.path.isfile(os.path.join(folder, f))
-
-            and f.lower().endswith(".pdb")
-
-        ]
-
+        fnames = [f for f in file_list if os.path.isfile(os.path.join(folder, f)) and f.lower().endswith(".pdb")]
         window["-FILE LIST-"].update(fnames)
 
     if event == '-Submit id-':
         print(values['-PDB ID-'])
         print(values['slider1'])
         print(values['slider2'])
+
     if event == '-Submit files-':
         print(values['-FOLDER-'])
         print(values['slider1'])
         print(values['slider2'])
+
     if event == 'Clear':
         window['-OUTPUT-'].update('')
-
 
 window.close()
